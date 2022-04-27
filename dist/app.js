ed1dace83e96d76b5bd4016f677075e7516ae4a7
ellielle <40385743+ellielle@users.noreply.github.com>
2022-04-27 11:06:17 -0400

    section 6 compelte

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,135 @@
 "use strict";
-let add;
-add = (n1, n2) => {
-    return n1 + n2;
-};
-class Person {
-    constructor(name) {
-        this.name = name;
-        if (name) {
-            this.name = name;
-        }
+// type Admin = {
+//   name: string;
+//   privileges: string[];
+// }
+//
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// }
+//
+// // interface Admin {
+// //   name: string;
+// //   privileges: string[];
+// // }
+// //
+// // interface Employee {
+// //   name: string;
+// //   startDate: Date;
+// // }
+// //
+// // interface ElevatedEmployee extends Admin, Employee {}
+//
+// type ElevatedEmployee = Admin & Employee;
+//
+// const employeeE1: ElevatedEmployee = {
+//   name: "Ellie",
+//   privileges: ["create-server"],
+//   startDate: new Date()
+// };
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
     }
-    greet(phrase) {
-        if (this.name) {
-            console.log(`${phrase} ${this.name}`);
-        }
-        else {
-            console.log("Hi");
-        }
-    }
+    return a + b;
 }
-let user1;
-// user1 = new Person("Ellie");
-user1 = new Person();
-user1.greet("Hi there, I am");
+const result = add("Max", "Schwarz");
+result.split(" ");
+const fetchedUserData = {
+    id: "u1",
+    name: "Max",
+    job: { title: "CEO", description: "MY COMPANY BITCH" }
+};
+console.log(fetchedUserData.job.title);
+const userInput = '';
+const storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
+console.log(storedData);
+//
+// type UnknownEmployee = Employee | Admin;
+// console.log(typeof employeeE1);
+//
+//
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//   console.log(`Name: ${emp.name}`);
+//   if ("privileges" in emp) {
+//     console.log(`Privileges ${emp.privileges}`);
+//   }
+//   if ("startDate" in emp) {
+//     console.log(`Start Date: ${emp.startDate}`);
+//   }
+// }
+//
+// printEmployeeInformation(employeeE1);
+//
+// class Car {
+//   drive() {
+//     console.log("Driving...");
+//   }
+// }
+//
+// class Truck {
+//   drive() {
+//     console.log("Driving a truck...");
+//   }
+//
+//   loadCargo(amount: number) {
+//     console.log("Loading cargo... " + amount);
+//   }
+// }
+//
+// type Vehicle = Car | Truck;
+//
+// const v1 = new Car();
+// const v2 = new Truck();
+//
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(32);
+//   }
+// }
+//
+// useVehicle(v1);
+// useVehicle(v2);
+//
+// //
+//
+// interface Bird {
+//   type: "bird";
+//   flyingSpeed: number;
+// }
+//
+// interface Horse {
+//   type: "horse";
+//   runningSpeed: number;
+// }
+//
+// type Animal = Bird | Horse;
+//
+// function moveAnimal(animal: Animal) {
+//   let speed;
+//   switch (animal.type) {
+//     case "bird":
+//       speed = animal.flyingSpeed;
+//       break;
+//     case "horse":
+//       speed = animal.runningSpeed;
+//       break;
+//   }
+//   console.log(`Moving at speed: ${speed}`);
+// }
+//
+// const userInputElement = document.getElementById("user-input")! as HTMLInputElement;
+// userInputElement.value = "hi there";
+//
+// //
+//
+// interface ErrorContainer { // { email: "Not a valid email", username: "Must start with a character" }
+//   [prop: string]: string;
+// }
+//
+// const errorBag: ErrorContainer = {
+//   email: "Not a valid email.",
+//   username: "Must start with a capital character!"
+// };
--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,146 @@
-// type AddFn = (a: number, b: number) => number;
-interface AddFn {
-  (a: number, b: number): number;
+// type Admin = {
+//   name: string;
+//   privileges: string[];
+// }
+//
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// }
+//
+// // interface Admin {
+// //   name: string;
+// //   privileges: string[];
+// // }
+// //
+// // interface Employee {
+// //   name: string;
+// //   startDate: Date;
+// // }
+// //
+// // interface ElevatedEmployee extends Admin, Employee {}
+//
+// type ElevatedEmployee = Admin & Employee;
+//
+// const employeeE1: ElevatedEmployee = {
+//   name: "Ellie",
+//   privileges: ["create-server"],
+//   startDate: new Date()
+// };
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+function add(a: string, b: string): string;
+function add(a: number, b: number): number;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
-let add: AddFn;
+const result = add("Max", "Schwarz");
+result.split(" ");
 
-add = (n1: number, n2: number) => {
-  return n1 + n2;
+const fetchedUserData = {
+  id: "u1",
+  name: "Max",
+  job: { title: "CEO", description: "MY COMPANY BITCH" }
 };
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
-}
+console.log(fetchedUserData.job.title);
 
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
+const userInput = '';
+const storedData = userInput ?? "DEFAULT";
 
-class Person implements Greetable {
-  constructor(public name?: string) {
-    if (name) {
-      this.name = name;
-    }
-  }
-
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(`${phrase} ${this.name}`);
-    } else {
-      console.log("Hi");
-    }
-  }
-}
-
-
-let user1: Person;
-
-// user1 = new Person("Ellie");
-user1 = new Person();
-
-
-user1.greet("Hi there, I am");
+console.log(storedData);
+//
+// type UnknownEmployee = Employee | Admin;
+// console.log(typeof employeeE1);
+//
+//
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//   console.log(`Name: ${emp.name}`);
+//   if ("privileges" in emp) {
+//     console.log(`Privileges ${emp.privileges}`);
+//   }
+//   if ("startDate" in emp) {
+//     console.log(`Start Date: ${emp.startDate}`);
+//   }
+// }
+//
+// printEmployeeInformation(employeeE1);
+//
+// class Car {
+//   drive() {
+//     console.log("Driving...");
+//   }
+// }
+//
+// class Truck {
+//   drive() {
+//     console.log("Driving a truck...");
+//   }
+//
+//   loadCargo(amount: number) {
+//     console.log("Loading cargo... " + amount);
+//   }
+// }
+//
+// type Vehicle = Car | Truck;
+//
+// const v1 = new Car();
+// const v2 = new Truck();
+//
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(32);
+//   }
+// }
+//
+// useVehicle(v1);
+// useVehicle(v2);
+//
+// //
+//
+// interface Bird {
+//   type: "bird";
+//   flyingSpeed: number;
+// }
+//
+// interface Horse {
+//   type: "horse";
+//   runningSpeed: number;
+// }
+//
+// type Animal = Bird | Horse;
+//
+// function moveAnimal(animal: Animal) {
+//   let speed;
+//   switch (animal.type) {
+//     case "bird":
+//       speed = animal.flyingSpeed;
+//       break;
+//     case "horse":
+//       speed = animal.runningSpeed;
+//       break;
+//   }
+//   console.log(`Moving at speed: ${speed}`);
+// }
+//
+// const userInputElement = document.getElementById("user-input")! as HTMLInputElement;
+// userInputElement.value = "hi there";
+//
+// //
+//
+// interface ErrorContainer { // { email: "Not a valid email", username: "Must start with a character" }
+//   [prop: string]: string;
+// }
+//
+// const errorBag: ErrorContainer = {
+//   email: "Not a valid email.",
+//   username: "Must start with a capital character!"
+// };
